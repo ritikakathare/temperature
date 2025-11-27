efce7aa980997aedee0169e87253ba3cf64603ac
ritikakathare <ritikakathare15@gmail.com>
2025-11-27 14:52:57 +0530

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,14 @@
+import sys
+
+script_name = sys.argv[0]   
+
+temp = float(sys.argv[1])
+
+if temp < 15:
+    result = "Cold"
+elif 15 <= temp <= 30:
+    result = "Normal"
+else:
+    result = "Hot"
+print("Temperature (°C):", temp)
+print("Condition:", result)
